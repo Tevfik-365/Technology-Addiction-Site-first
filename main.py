f69de7d85893ac0fb69783b051407b11c4b38635
Tevfik <tevfiktzn@gmail.com>
2024-10-28 13:00:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from flask import Flask
 import random
 
+
 app = Flask(__name__)
-facts_list = ["Elon Musk, sosyal ağların içeriği görüntülemek için mümkün olduğunca fazla zaman harcamamız için bizi platformun içinde tutmak üzere tasarlandığını iddia ediyor.",
-            "2018 yılında yapılan bir araştırmaya göre 18-34 yaş arası kişilerin %50'den fazlası akıllı telefonlarına bağımlı olduğunu düşünüyor.",
-            "Sosyal ağların olumlu ve olumsuz yanları var ve bu platformları kullanırken her ikisinin de farkında olmalıyız.",
-            "Teknoloji bağımlılığı çalışması, modern bilimsel araştırmanın en alakalı alanlarından biridir."]
+facts_list = ["Elon Musk claims that social networks are designed to keep us inside the platform to spend as much time as possible viewing content.",
+"According to a 2018 study, more than 50% of people aged 18-34 think they are addicted to their smartphones.",
+"Social networks have positive and negative sides and we should be aware of both when using these platforms.",
+"The study of technology addiction is one of the most relevant areas of modern scientific research."]
+
 
 @app.route("/")
 def index():
-    return f'<h1>MERHABA! Bu sayfada, teknolojik bağımlılıklar hakkında birkaç ilginç gerçeği öğrenebilirsiniz!<a href="/rastgele_gercek">Rastgele bir gerçeği görüntüle!</a>'
+return f '<h1>HELLO! On this page, you can learn a few interesting facts about technological addictions!<a href="/random_fact">View a random fact!</a>'
 
-@app.route("/rastgele_gercek")
+
+@app.route("/random_real")
 def facts():
-    return f'<h1>{random.choice(facts_list)}</h1>'
+return f'<h1>{random.choice(facts_list)}</h1>'
+
 
 @app.route("/t")
 def slm():
-    return f'<h1>slm ben tifo</h1>'
+return f'<h1>selam ben tifo</h1>'
 app.run(debug=True)
